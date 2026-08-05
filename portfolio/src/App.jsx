e0db56d02a64c5093dbf8f8a1244375e80d78dfd
Joaquin Meroy <joaquinameroy@gmail.com>
2026-08-05 13:23:23 +0800

    Updates
    Updates

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import SplitText from "./component/common/SplitText.jsx";
+import Navbar from "./component/Navbar.jsx";
+const handleAnimationComplete = () => {
+  console.log("All letters have animated!");
+};
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio/src/component/Navbar.jsx
@@ -0,0 +1,30 @@
+import PillNav from "./common/PillNav";
+import logo from "../assets/react.svg";
+
+function Navbar() {
+  return (
+    <div>
+      <PillNav
+        logo={logo}
+        logoAlt="Company Logo"
+        items={[
+          { label: "Home", href: "/" },
+          { label: "About", href: "/about" },
+          { label: "Services", href: "/services" },
+          { label: "Contact", href: "/contact" },
+        ]}
+        activeHref="/"
+        className="custom-nav"
+        ease="power2.easeOut"
+        baseColor="#000000"
+        pillColor="#ffffff"
+        hoveredPillTextColor="#ffffff"
+        pillTextColor="#000000"
+        theme="light"
+        initialLoadAnimation={false}
+      />
+    </div>
+  );
+}
+
+export default Navbar;
